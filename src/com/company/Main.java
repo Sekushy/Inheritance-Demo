ea9d9cedb79ce5f14d53de24c2c3b2ea4a49f09e
Draghici Andrei <draghici.andrei095@gmail.com>
2021-04-02 20:39:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> pressenceList = new ArrayList<>();
+        pressenceList.add("Andrei");
+
+        System.out.println(pressenceList);
+    }
+}
