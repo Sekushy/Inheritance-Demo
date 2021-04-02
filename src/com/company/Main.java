21eccbf345cba9530ced954c3b409d85e467edb7
Draghici Andrei <draghici.andrei095@gmail.com>
2021-04-02 20:43:23 +0300

    Made changes to main added new student

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> pressenceList = new ArrayList<>();
         pressenceList.add("Andrei");
+        pressenceList.add("Cosmin");
 
         System.out.println(pressenceList);
     }
